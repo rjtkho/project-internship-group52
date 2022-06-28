c9040868a4e26506014c13b2a67f1bbc173384a7
unknown <rajatkhobragade00@gmail.com>
2022-06-28 16:45:25 +0530

    new

---
+++ src/routes/route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+ const collegeController = require("../controller/collegeController")
+ const internsController = require("../controller/interController")
+
+
+
+router.post('/functionup/colleges', )
+
+router.post('/functionup/interns',)
+
+router.get('/functionup/collegeDetails', )
+
+module.exports = router;

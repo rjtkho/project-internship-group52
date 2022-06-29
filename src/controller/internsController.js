9e295de03bcb95c56eb8ea3c44a1a8c3e49cb099
unknown <rajatkhobragade00@gmail.com>
2022-06-29 15:03:35 +0530

    new commit

--- src/controller/internsController.js
+++ src/controller/internsController.js
@@ -1,10 +1,23 @@
+const  mongoose  = require("mongoose")
 const collegeModel=require("../models/collegeModel")
 const internModel = require("../models/internModel")
+
+//================================================Create interns details======================================================
 const createInternDetails = async function (req, res) {
     try {
         const id = req.body.collegeId
         let data = { name, mobile, email, collegeName, } = req.body
-    
+        
+        const checkMobileNumber = await internModel.findOne({ mobile: mobile});
+        if (checkMobileNumber) {
+            return res.status(400).send({ status: false, msg: ` ${mobile} mobile number  already exist please enter another mobile number` })
+        }
+
+        const checkEmailId = await internModel.findOne({ email: email});
+        if (checkEmailId) {
+            return res.status(400).send({ status: false, msg: ` ${email} email already exists please enter another another email` })
+        }
+
         const findCollege = await collegeModel.findById(id)
         if (!findCollege) {
             return res.status(400).send({status: false, data: "college not exists"})
@@ -18,6 +31,6 @@ const createInternDetails = async function (req, res) {
     }
 }
 
-//================================================POST interns======================================================
+//================================================Create interns details======================================================
 
 module.exports.createInternDetails=createInternDetails;
